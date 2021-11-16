c5cc45f4e68455ad2692e5ba7f1477e0bf118b15
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2021-11-16 09:26:13 +0100

    add check for json extension

--- scripts/validate_jsons.R
+++ scripts/validate_jsons.R
@@ -7,13 +7,19 @@ validate_jsons <- function(files, schema){
               greedy = TRUE)
 }
 
+files <- list.files(
+  path = here::here("blogs"), 
+  full.names = TRUE
+)
+ext <- grep("json$", files, invert = TRUE)
+if(length(ext) > 0)
+  stop("File has wrong extention. Please rename to end with 'json'\n",
+       paste0(basename(files[grep("json$", files, invert = TRUE)]), collapse ="\n"), 
+       call. = FALSE)
 
 #  Validate blog json
 validate_jsons(
-  list.files(
-    path = here::here("blogs"), 
-    full.names = TRUE
-  ),
+  files,
   here::here("scripts/.entry_schema.json")
 )
 
